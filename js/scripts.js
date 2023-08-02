1c27adda555f576e1e51edb4408c75f547c623c1
Muhammad88IT <139487361+Muhammad88IT@users.noreply.github.com>
2023-08-02 11:47:19 +0200

    Cleared previous codes; Created a pokemonList variable
    The variable contains a list of arrays of Pokemon characters!

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
-// alert call
-alert('Hello World');
-
-// creating variable
-let favouriteFood = 'Pizza';
-document.write(favouriteFood)
+let pokemonList = [
+    {name:'Bulbasaur', height:0.7, types: ['GRASS', 'POISON']},
+    {name:'Charizard', height:1.7, types: ['FIRE', 'FLYING']},
+    {name:'Butterfree', height:1.1, types: ['BUG', 'FLYING']},
+    {name:'Beedrill', height:1, types: ['BUG','POISON']},
+    {name:'Pidgeot', height:1.5, types: ['FLYING','NORMAL']},
+];
