c0ddbae00bb89c0280e73a6b76159bf688248f07
Muhammad88IT <139487361+Muhammad88IT@users.noreply.github.com>
2023-08-05 18:56:13 +0200

    Added list of items and created loop to display the items

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,36 @@
 let pokemonList = [
-    {name:'Bulbasaur', height:0.7, types: ['GRASS', 'POISON']},
-    {name:'Charizard', height:1.7, types: ['FIRE', 'FLYING']},
-    {name:'Butterfree', height:1.1, types: ['BUG', 'FLYING']},
-    {name:'Beedrill', height:1, types: ['BUG','POISON']},
-    {name:'Pidgeot', height:1.5, types: ['FLYING','NORMAL']},
+    {
+        name: 'Bulbasaur',
+        height: 0.7,
+        types: ['GRASS', 'POISON']
+    },
+    {
+        name: 'Charizard',
+        height: 1.7,
+        types: ['FIRE', 'FLYING']
+    },
+    {
+        name: 'Butterfree',
+        height: 1.1,
+        types: ['BUG', 'FLYING']
+    },
+    {
+        name: 'Beedrill',
+        height: 1,
+        types: ['BUG', 'POISON']
+    },
+    {
+        name: 'Pidgeot',
+        height: 1.5,
+        types: ['FLYING', 'NORMAL']
+    }
 ];
+
+for (i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height <= 1.5) {
+        document.write(pokemonList[i].name + ' ' + '(height:' + pokemonList[i].height + ')' + ' ');
+    }
+    else {
+        document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - Wow, that\'s big!' + ' ');
+    }
+}
