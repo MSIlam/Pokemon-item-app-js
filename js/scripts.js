481f85b546d8906c317e5b1e669c8a872a65c3d0
Muhammad88IT <139487361+Muhammad88IT@users.noreply.github.com>
2023-08-13 17:58:57 +0200

    Updated the syntax

--- js/scripts.js
+++ js/scripts.js
@@ -1,55 +1,3 @@
-
-
-// let pokemonRepository = (function () {
-//     let pokemonList = [
-//         {
-//             name: 'Bulbasaur',
-//             height: 0.7,
-//             types: ['GRASS', 'POISON']
-//         },
-//         {
-//             name: 'Charizard',
-//             height: 1.7,
-//             types: ['FIRE', 'FLYING']
-//         },
-//         {
-//             name: 'Butterfree',
-//             height: 1.1,
-//             types: ['BUG', 'FLYING']
-//         },
-//         {
-//             name: 'Beedrill',
-//             height: 1,
-//             types: ['BUG', 'POISON']
-//         },
-//         {
-//             name: 'Pidgeot',
-//             height: 1.5,
-//             types: ['FLYING', 'NORMAL']
-//         }];
-
-//      function add(pokemon) {
-//          pokemonList.push(pokemon);
-//      }
-//      function getAll() {
-//          return pokemonList;
-//      }
-//      return {
-//          getAll:getAll,
-//          add:add
-//      }
-//  }) ();
-
-// pokemonRepository.getAll().forEach (function(pokemon) {
-// document.write(pokemon);
-//  });
-
-//  pokemonRepository.add({name:'pikachu', height:1.04, types:['Electric']})
-
-//  pokemonRepository.getAll().forEach (function(pokemon) {
-//     document.write(pokemon);
-//      });
-
 let pokemonRepository = (function() {
   let pokemonList = [
    {
@@ -101,7 +49,7 @@ pokemonRepository.getAll().forEach(function(pokemon) {
  document.write(pokemon);
 });
 
-pokemonRepository.add({name: "Othmanizard", height: 1.7, types: ["CODER"]})
+pokemonRepository.add({name:'pikachu', height:1.04, types:['Electric']})
 
 document.write("Show All including new one")
 pokemonRepository.getAll().forEach(function(pokemon) {
