2ff6d4d9a61864672f208cff9ddb4e2d10635e4c
Muhammad88IT <139487361+Muhammad88IT@users.noreply.github.com>
2023-08-16 18:39:39 +0200

    Updated the function to create a pokemon list framework

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 let pokemonRepository = (function() {
+  // a variable containing the pokemon item list
   let pokemonList = [
    {
      name: 'Bulbasaur',
@@ -25,27 +26,46 @@ let pokemonRepository = (function() {
      height: 1.5,
      types: ['FLYING', 'NORMAL']
    }];
- 
+//  a function for adding new pokemon in the list given the conditions are met
  function add(pokemon) {
 if (typeof pokemon === 'object' &&
-['name', 'height','types']) {
+'name', 'height','types' in pokemon) {
   pokemonList.push(pokemon);
 } else {
   console.log('I dont know');
 }
 }
-
+// a function for retriving the list of pokemon
  function getAll() {
    return pokemonList;
+ } 
+// function creating the HTML framework for the pokemon list
+ function addListItem (pokemon) {
+  const pokemonUl = document.querySelector ('.pokemon-list');
+  const listitem = document.createElement ('li');
+  const button = document.createElement ('button');
+  button.innerText= pokemon.name;
+  button.classList.add ('pokemon-button');
+  listitem.appendChild (button);
+  pokemonUl.appendChild (listitem);
+  button.addEventListener ('click', function(event) {
+    showDetails(pokemon)
+  });
  }
+// function for showing pokemon name in the console
+ function showDetails(pokemon) {
+  console.log(pokemon.name);
+ }
+// making the IIFE functions available
  return {
    getAll: getAll,
-   add: add
+   add: add,
+   addListItem:addListItem
  }
 })();
-
-pokemonRepository.add({name:'pikachu', height:1.04, types:['Electric']})
-
+// adding a new pokemon character in the original list
+pokemonRepository.add({name:'Pikachu', height:1.04, types:['Electric']})
+// calling the pokemon list items from the repository
 pokemonRepository.getAll().forEach(function(pokemon) {
-  document.write('<p>'+pokemon.name+' - '+pokemon.height+ ' - '+pokemon.types+'</p>');
+  pokemonRepository.addListItem(pokemon);
 });
