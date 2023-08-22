5c4caab561bcb9a8b2b4c5b6932d3fa47ce91a66
Muhammad88IT <139487361+Muhammad88IT@users.noreply.github.com>
2023-08-22 10:00:45 +0200

    Removed the code for logging all the pokemon list in the console

--- js/scripts.js
+++ js/scripts.js
@@ -33,14 +33,13 @@ let pokemonRepository = (function() {
 function loadList() {
   return fetch (apiURL).then (function (response){
   return response.json();
-    }).then (function (json) {
+    }).then(function (json) {
     json.results.forEach(function (item) {
     let pokemon = {
       name:item.name,
       detailsURL:item.url
     };
     add(pokemon);
-    console.log(pokemon);
   });
 }).catch (function (e){
   console.error (e);
