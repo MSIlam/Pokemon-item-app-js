8e46bb550349052553e69664b8ade54e208a5733
Muhammad88IT <139487361+Muhammad88IT@users.noreply.github.com>
2023-09-17 15:24:16 +0200

    Made some more adjustments

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
-let pokemonRepository = (function () {
-  let modalContainer = document.querySelector("#modal-container");
-  let pokemonList = [];
-  let apiURL = "https://pokeapi.co/api/v2/pokemon/?limit=100";
+const pokemonRepository = (function () {
+  const pokemonList = [];
+  const apiURL = "https://pokeapi.co/api/v2/pokemon/?limit=100";
 
   //  a function for adding new pokemon in the list given the conditions are met
   function add(pokemon) {
@@ -19,23 +18,28 @@ let pokemonRepository = (function () {
 
   // function creating the HTML framework for the pokemon list
   function addListItem(pokemon) {
-    let pokemonUl = document.querySelector(".list-group");
-    let listitem = document.createElement("li");
-    let imgContainer = document.createElement("div");
-    let Image = document.createElement("img");
-    let button = document.createElement("button");
+    const pokemonUl = document.querySelector(".list-group");
+    const listitem = document.createElement("li");
+    const imgContainer = document.createElement("div");
+    const image = document.createElement("img");
+    const button = document.createElement("button");
+
     button.classList.add("search-btn");
     button.innerText = "Show details";
     button.setAttribute("data-target", "#exampleModal");
     button.setAttribute("data-toggle", "modal");
+
     listitem.classList.add("list-group-item");
-    Image.setAttribute("src", pokemon.imageUrl);
-    Image.setAttribute("width", 100);
-    imgContainer.appendChild(Image);
     listitem.innerText = pokemon.name.toUpperCase();
     listitem.appendChild(imgContainer);
     listitem.appendChild(button);
+
+    image.setAttribute("src", pokemon.imageUrl);
+    image.setAttribute("width", 100);
+
+    imgContainer.appendChild(Image);
     pokemonUl.appendChild(listitem);
+
     button.addEventListener("click", function (event) {
       showDetails(pokemon);
     });
@@ -63,7 +67,7 @@ let pokemonRepository = (function () {
   }
 
   function loadDetails(pokemon) {
-    let url = pokemon.detailsURL;
+    const url = pokemon.detailsURL;
     return fetch(url)
       .then(function (response) {
         return response.json();
@@ -96,30 +100,29 @@ let pokemonRepository = (function () {
     // lets add content to the new bootstrap modal
 
     // first select the contents inside the modal
-    let modalHeader = $(".modal-header");
-    let modalTitle = $(".modal-title");
-    let modalBody = $(".modal-body");
+    const modalTitle = $(".modal-title");
+    const modalBody = $(".modal-body");
 
     // now empty the contents that needs to be filled
     modalBody.empty();
     modalTitle.empty();
 
     // create element for name
-    let namePokemon = $("<h1>" + pokemon.name.toUpperCase() + "</h1>");
+    const namePokemon = $("<h1>" + pokemon.name.toUpperCase() + "</h1>");
     // create image in modal
-    let imageFront = $("<img class='modal-img'>");
+    const imageFront = $("<img class='modal-img'>");
     imageFront.attr("src", pokemon.imageUrl);
 
-    let imageBack = $("<img class= 'modal-img2'>");
+    const imageBack = $("<img class= 'modal-img2'>");
     imageBack.attr("src", pokemon.imageUrlBack);
 
-    let heightElement = $("<p>" + "Height: " + pokemon.height + "</p>");
+    const heightElement = $("<p>" + "Height: " + pokemon.height + "</p>");
 
-    let weightElement = $("<p>" + "Weight: " + pokemon.weight + "</p>");
+    const weightElement = $("<p>" + "Weight: " + pokemon.weight + "</p>");
 
-    let typesElement = $("<p>" + "Types: " + pokemon.types + "</p>");
+    const typesElement = $("<p>" + "Types: " + pokemon.types + "</p>");
 
-    let abilitiesElement = $(
+    const abilitiesElement = $(
       "<p>" + "Abilities: " + pokemon.abilities + "</p>"
     );
 
